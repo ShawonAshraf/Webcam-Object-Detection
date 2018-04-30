99296db85f8b0c0b9071a466598549ae76b5bcbe
Shawon Ashraf <shawon13@live.com>
2018-04-30 23:03:15 +0600

    babel gen

--- application/webcam.js
+++ application/webcam.js
@@ -27,86 +27,88 @@
  */
 
 import * as tf from '@tensorflow/tfjs';
+require("babel-core/register");
+require("babel-polyfill");
 
 /**
  * A class that wraps webcam video elements to capture Tensor4Ds.
  */
 export class Webcam {
-  /**
-   * @param {HTMLVideoElement} webcamElement A HTMLVideoElement representing the webcam feed.
-   */
-  constructor(webcamElement) {
-    this.webcamElement = webcamElement;
-  }
-
-  /**
-   * Captures a frame from the webcam and normalizes it between -1 and 1.
-   * Returns a batched image (1-element batch) of shape [1, w, h, c].
-   */
-  capture() {
-    return tf.tidy(() => {
-      // Reads the image as a Tensor from the webcam <video> element.
-      const webcamImage = tf.fromPixels(this.webcamElement);
-
-      // Crop the image so we're using the center square of the rectangular
-      // webcam.
-      const croppedImage = this.cropImage(webcamImage);
-
-      // Expand the outer most dimension so we have a batch size of 1.
-      const batchedImage = croppedImage.expandDims(0);
-
-      // Normalize the image between -1 and 1. The image comes in between 0-255,
-      // so we divide by 127 and subtract 1
-      return batchedImage.toFloat().div(tf.scalar(255));
-    });
-  }
-
-  /**
-   * Crops an image tensor so we get a square image with no white space.
-   * @param {Tensor4D} img An input image Tensor to crop.
-   */
-  cropImage(img) {
-    const size = Math.min(img.shape[0], img.shape[1]);
-    const centerHeight = img.shape[0] / 2;
-    const beginHeight = centerHeight - (size / 2);
-    const centerWidth = img.shape[1] / 2;
-    const beginWidth = centerWidth - (size / 2);
-    return img.slice([beginHeight, beginWidth, 0], [size, size, 3]);
-  }
-
-  /**
-   * Adjusts the video size so we can make a centered square crop without
-   * including whitespace.
-   * @param {number} width The real width of the video element.
-   * @param {number} height The real height of the video element.
-   */
-  adjustVideoSize(width, height) {
-    const aspectRatio = width / height;
-    if (width >= height) {
-      this.webcamElement.width = aspectRatio * this.webcamElement.height;
-    } else if (width < height) {
-      this.webcamElement.height = this.webcamElement.width / aspectRatio;
+    /**
+     * @param {HTMLVideoElement} webcamElement A HTMLVideoElement representing the webcam feed.
+     */
+    constructor(webcamElement) {
+        this.webcamElement = webcamElement;
     }
-  }
 
-  async setup() {
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        'audio': false,
-        'video': {facingMode: 'environment'}
-      });
-      window.stream = stream;
-      this.webcamElement.srcObject = stream;
-      return new Promise(resolve => {
-        this.webcamElement.onloadedmetadata = () => {
-          this.adjustVideoSize(
-            this.webcamElement.videoWidth,
-            this.webcamElement.videoHeight);
-          resolve();
-        };
-      });
-    } else {
-      throw new Error('No webcam found!');
+    /**
+     * Captures a frame from the webcam and normalizes it between -1 and 1.
+     * Returns a batched image (1-element batch) of shape [1, w, h, c].
+     */
+    capture() {
+        return tf.tidy(() => {
+            // Reads the image as a Tensor from the webcam <video> element.
+            const webcamImage = tf.fromPixels(this.webcamElement);
+
+            // Crop the image so we're using the center square of the rectangular
+            // webcam.
+            const croppedImage = this.cropImage(webcamImage);
+
+            // Expand the outer most dimension so we have a batch size of 1.
+            const batchedImage = croppedImage.expandDims(0);
+
+            // Normalize the image between -1 and 1. The image comes in between 0-255,
+            // so we divide by 127 and subtract 1
+            return batchedImage.toFloat().div(tf.scalar(255));
+        });
     }
-  }
+
+    /**
+     * Crops an image tensor so we get a square image with no white space.
+     * @param {Tensor4D} img An input image Tensor to crop.
+     */
+    cropImage(img) {
+        const size = Math.min(img.shape[0], img.shape[1]);
+        const centerHeight = img.shape[0] / 2;
+        const beginHeight = centerHeight - (size / 2);
+        const centerWidth = img.shape[1] / 2;
+        const beginWidth = centerWidth - (size / 2);
+        return img.slice([beginHeight, beginWidth, 0], [size, size, 3]);
+    }
+
+    /**
+     * Adjusts the video size so we can make a centered square crop without
+     * including whitespace.
+     * @param {number} width The real width of the video element.
+     * @param {number} height The real height of the video element.
+     */
+    adjustVideoSize(width, height) {
+        const aspectRatio = width / height;
+        if (width >= height) {
+            this.webcamElement.width = aspectRatio * this.webcamElement.height;
+        } else if (width < height) {
+            this.webcamElement.height = this.webcamElement.width / aspectRatio;
+        }
+    }
+
+    async setup() {
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            const stream = await navigator.mediaDevices.getUserMedia({
+                'audio': false,
+                'video': { facingMode: 'environment' }
+            });
+            window.stream = stream;
+            this.webcamElement.srcObject = stream;
+            return new Promise(resolve => {
+                this.webcamElement.onloadedmetadata = () => {
+                    this.adjustVideoSize(
+                        this.webcamElement.videoWidth,
+                        this.webcamElement.videoHeight);
+                    resolve();
+                };
+            });
+        } else {
+            throw new Error('No webcam found!');
+        }
+    }
 }
