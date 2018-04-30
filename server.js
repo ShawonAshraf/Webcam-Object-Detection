d3976f7b7d572a6fdff6df59db20a4dd7c2ef508
Shawon Ashraf <shawon13@live.com>
2018-04-30 21:51:48 +0600

    fixed compatibility

--- server.js
+++ server.js
@@ -11,15 +11,6 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
-// maintenance middleware
-app.use((req, res, next) => {
-    res.render('maintenance.hbs', resObj.responseObject(
-        'We\'ll be back soon, please drop by again!',
-        'Site under maintenance'
-    ));
-});
-
-
 // routes
 app.get('/', (req, res) => {
     res.render('home.hbs', resObj.responseObject(
