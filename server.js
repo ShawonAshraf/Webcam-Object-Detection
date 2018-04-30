6aeb9d20a6273ae3b2b0c1abdd12c5d11ceb928a
Shawon Ashraf <shawon13@live.com>
2018-04-30 21:23:00 +0600

    added maintenance.hbs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import * as hbs from 'hbs';
+import * as resObj from './response-obj/response-obj';
 
 
 const port = process.env.PORT || 3000;
@@ -12,5 +13,8 @@ app.use(express.static(__dirname + '/public'));
 
 // maintenance middleware
 app.use((req, res, next) => {
-    res.render('maintenance.hbs');
+    res.render('maintenance.hbs', resObj.responseObject(
+        'We\'ll be back soon, please drop by again!',
+        'Site under maintenance'
+    ));
 });
